61f1cfb1153b989a6ce7ffbe652b33e081298378
fukushige <k.fukushige@cdrive-inc.jp>
2026-06-17 18:09:33 +0800

    add: 店舗一覧　tablet画面の作成

--- src/js/main.js
+++ src/js/main.js
@@ -114,24 +114,24 @@ jQuery(function($) {
         updateScrollTopButton();
     }
 
-    /**
-     * vw算出(SP)
-     */
-    const valueSP = (12.67) / 390;
-    const resultSP = Math.floor(valueSP * 10000) / 100;
-    console.log('SP: ' + resultSP.toFixed(2) + 'vw');
+    // /**
+    //  * vw算出(SP)
+    //  */
+    // const valueSP = (56) / 390;
+    // const resultSP = Math.floor(valueSP * 10000) / 100;
+    // console.log('SP: ' + resultSP.toFixed(2) + 'vw');
 
-    /**
-     * vw算出(Tablet)
-     */
-    const valueTablet = (20) / 768;
-    const resultTablet = Math.floor(valueTablet * 10000) / 100;
-    console.log('Tablet: ' + resultTablet.toFixed(2) + 'vw');
+    // /**
+    //  * vw算出(Tablet)
+    //  */
+    // const valueTablet = (706.95) / 768;
+    // const resultTablet = Math.floor(valueTablet * 10000) / 100;
+    // console.log('Tablet: ' + resultTablet.toFixed(2) + 'vw');
 
     /**
      * vw算出(PC)
      */
-    const valuePC = (10) / 1920;
+    const valuePC = (20) / 1920;
     const resultPC = Math.floor(valuePC * 10000) / 100;
     console.log('PC: ' + resultPC.toFixed(2) + 'vw');
 });
