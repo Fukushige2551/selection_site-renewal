8643386465451db653169427f6c1795d8b2b0403
fukushige <k.fukushige@cdrive-inc.jp>
2026-07-28 13:48:23 +0900

    add: デフォルトテンプレートの宣言コード追加

--- page-select.php
+++ page-select.php
@@ -1,4 +1,10 @@
-<?php get_header(); ?>
+<?php
+/*
+Template Name: セレクションのこだわり
+*/
+
+get_header();
+?>
 
 <main id="page-select" class="p-page-select">
     <nav class="c-breadcrumb" aria-label="パンくずリスト">
