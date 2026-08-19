546ed83a66f514fb395d93f8e0e6e13edc5de5dc
fukushige <k.fukushige@cdrive-inc.jp>
2026-08-19 17:04:38 +0900

    fix: バイヤーメッセージのblob削除

--- template-parts/select-detail/message.php
+++ template-parts/select-detail/message.php
@@ -15,20 +15,12 @@ $message_background_decorations = array_values(array_filter(
             <img class="<?php echo esc_attr($block_class . '__message--backgroundDecoration ' . $block_class . '__message--backgroundDecoration-' . sanitize_html_class($decoration['key']) . ' ' . $block_class . '__message--backgroundDecoration-legacyAnchor'); ?>" src="<?php echo esc_url(foods_get_select_detail_asset_url($config, $decoration['src'])); ?>" alt="<?php echo esc_attr($decoration['alt'] ?? ''); ?>">
         <?php endif; ?>
     <?php endforeach; ?>
-    <div class="<?php echo esc_attr($block_class); ?>__message__bg">
-        <div class="<?php echo esc_attr($block_class); ?>__message__header">
-            <div class="<?php echo esc_attr($block_class); ?>__message--bg">
+    <div class="<?php echo esc_attr($block_class); ?>__message--bg">
                 <div class="<?php echo esc_attr($block_class); ?>__message--shape" aria-hidden="true">
                     <span class="<?php echo esc_attr($block_class); ?>__message--shapeBase"></span>
                     <span class="<?php echo esc_attr($block_class); ?>__message--shapeTop"></span>
                     <span class="<?php echo esc_attr($block_class); ?>__message--shapeMiddle"></span>
                     <span class="<?php echo esc_attr($block_class); ?>__message--shapeBottom"></span>
-                    <span class="<?php echo esc_attr($block_class); ?>__message--blob <?php echo esc_attr($block_class); ?>__message--blob-top"></span>
-                    <span class="<?php echo esc_attr($block_class); ?>__message--blob <?php echo esc_attr($block_class); ?>__message--blob-bottom"></span>
-                    <span class="<?php echo esc_attr($block_class); ?>__message--blob <?php echo esc_attr($block_class); ?>__message--blob-l1"></span>
-                    <span class="<?php echo esc_attr($block_class); ?>__message--blob <?php echo esc_attr($block_class); ?>__message--blob-l2"></span>
-                    <span class="<?php echo esc_attr($block_class); ?>__message--blob <?php echo esc_attr($block_class); ?>__message--blob-r1"></span>
-                    <span class="<?php echo esc_attr($block_class); ?>__message--blob <?php echo esc_attr($block_class); ?>__message--blob-r2"></span>
                 </div>
 
                 <img class="<?php echo esc_attr($block_class); ?>__message--bubbleSp" src="<?php echo esc_url(foods_get_select_detail_asset_url($config, $message['bubble_sp'])); ?>" alt="吹き出し">
@@ -47,7 +39,5 @@ $message_background_decorations = array_values(array_filter(
                     <p class="<?php echo esc_attr($block_class); ?>__message--textSp"><?php echo wp_kses_post($message['text']); ?></p>
                     <p class="<?php echo esc_attr($block_class); ?>__message--textPc"><?php echo wp_kses_post($message['text_pc']); ?></p>
                 </div>
-            </div>
-        </div>
     </div>
 </section>
